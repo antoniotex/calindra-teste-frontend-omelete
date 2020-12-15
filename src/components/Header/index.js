859ced060b8f8030d39ec2d9b99597000497870d
Antonio Carlos <antoniotx.dev@gmail.com>
2020-12-14 21:17:58 -0300

    Estiliza header e busca

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,11 @@
 import React, { useContext, useState } from 'react'
 import { ProductsContext } from '../../contexts/productsContext'
 import Loading from '../Loading'
+import logo from '../../assets/images/logo-omelete.svg'
+import './style.css'
 
 const Header = () => {
-    const [query, setQuery] = useState('')
+    const [query, setQuery] = useState('batman')
 
     const { searchProducts } = useContext(ProductsContext)
 
@@ -13,13 +15,12 @@ const Header = () => {
     }
 
     return (
-        <nav>
-            <div className='logo'>LOGO</div>
-            <form onSubmit={handleSubmit}>
-                <input type='text' name='search' value={query} onChange={e => setQuery(e.target.value)} />
-                <input type='submit' value='Buscar' />
+        <nav className='header'>
+            <img src={logo} className='header__logo' alt='Logo da Omelete Store.' title='Logo da Omelete Store' />
+            <form className='search' onSubmit={handleSubmit}>
+                <input className='search__query' type='text' name='search' value={query} onChange={e => setQuery(e.target.value)} />
+                <input className='search__submit' type='submit' value='Buscar' />
             </form>
-            <Loading/>
         </nav>
     )
 }
