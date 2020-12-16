b165c4c42d3cf7ad886b310143f12d35489678ba
Antonio Carlos <antoniotx.dev@gmail.com>
2020-12-16 20:25:26 -0300

    Corrigu bug de proxy

--- setupProxy.js
+++
@@ -1,8 +0,0 @@
-const { createProxyMiddleware } = require('http-proxy-middleware')
-
-module.exports = function(app){
-    app.use(createProxyMiddleware('/autocomplete', {
-        target: 'https://store.omelete.com.br',
-        changeOrigin: true
-    }))
-}
